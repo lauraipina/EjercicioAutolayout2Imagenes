aaa02aa5b6c1517102d3965e80e75fad5d68505d
Laura Iglesias <laura.iglesias.pina@gmail.com>
2015-01-23 11:36:11 +0100

    Initial Commit

---
+++ LIPEjercicioAutolayout2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LIPEjercicioAutolayout2
+//
+//  Created by Laura Iglesias Piña on 23/1/15.
+//  Copyright (c) 2015 lip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LIPEjercicioAutolayout2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LIPEjercicioAutolayout2
+//
+//  Created by Laura Iglesias Piña on 23/1/15.
+//  Copyright (c) 2015 lip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
